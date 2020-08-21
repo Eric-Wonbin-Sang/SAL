366eeafc0fd679f629b9df19988228f7efdfa7d8
eric.wonbin.sang <eric.wonbin.sang@gmail.com>
2020-08-21 01:56:20 -0400

    The Section class stores info about the section parsed from the roomsched website.

--- lib/StevensIT/Section.py
+++ lib/StevensIT/Section.py
@@ -1,48 +1,35 @@
-import datetime
-
-from General import Functions
-
 
 class Section:
 
-    def __init__(self, day, room, html_chunk, day_code_dict):
-        """
-        str day
-        str html_chunk: initial html part for parsing
-        """
+    def __init__(self, name, student_count, professor_name, room_name, day_code, start_time, end_time):
 
-        self.day = day
-        self.room = room
-        self.html_chunk = html_chunk
-        self.day_code_dict = day_code_dict
+        self.name = name
+        self.student_count = student_count
+        self.professor_name = professor_name
+        self.room_name = room_name
 
-        self.course_name, self.professor, self.start_time, self.end_time = self.get_data()
+        self.day_code = day_code
+        self.start_time = start_time
+        self.end_time = end_time
 
-    def get_data(self):
-        line = self.html_chunk.split("colspan=")[1]
-        raw_course_name = line.split("<br>")[0].split(">")[1].split("(")[0]
-        raw_professor, raw_times = line.split("<br>")[1].split("[")
+        self.subject_code, self.course_code, self.section_code = self.get_codes()
 
-        raw_times = raw_times[:-1].split("-")
-        raw_times[1] = raw_times[1].split("]")[0]
+        self.room = None
 
-        course_name = raw_course_name
-        professor = raw_professor
-        start_time = datetime.time(int(raw_times[0][:-2]), int(raw_times[0][-2:]))
-        end_time = datetime.time(int(raw_times[1][:-2]), int(raw_times[1][-2:]))
+        self.subject = None
+        self.course = None
 
-        return course_name, professor, start_time, end_time
+    def get_codes(self):
+        subject_code = (split_data := self.name.split(" "))[0]
+        course_code, section_code = split_data[1][:3], split_data[1][3:]
+        return subject_code, course_code, section_code
 
-    def __str__(self, do_one_line=False):
-        if not do_one_line:
-            return "Course Name: {}\nProfessor: {}\nStart Time: {}\nEnd Time: {}\n".format(
-                self.course_name,
-                self.professor,
-                self.start_time,
-                self.end_time)
-        return "{}: {} - {} | {} ({})".format(
-            self.day_code_dict[self.day.lower().lower()][:3].title(),
-            Functions.get_pretty_time(self.start_time),
-            Functions.get_pretty_time(self.end_time),
-            self.course_name,
-            self.professor)
+    def __str__(self):
+        return "{} {} {}-{}: start: {}  end: {}".format(
+            self.room_name,
+            self.subject_code,
+            self.course_code,
+            self.section_code,
+            self.start_time,
+            self.end_time
+        )
