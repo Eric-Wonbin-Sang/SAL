5c1a7cb7678f558bf953750714893b97ae6caa0a
eric.wonbin.sang <eric.wonbin.sang@gmail.com>
2020-08-24 15:37:41 -0400

    Reformatted the sheet_to_simple_response for the LTSheet class.

--- Classes/LTSheet.py
+++ Classes/LTSheet.py
@@ -1,13 +1,13 @@
 import pandas
 
-from General import ListHelp
+from General import ListHelp, Functions
 
 
 class LTSheet:
 
     """
-    This is an object  Google Sheet with a specific format that has a table of information on one side and a dictionary for
-    parameters
+    This is an object  Google Sheet with a specific format that has a table of information on one side and a dictionary
+    for parameters.
     """
 
     def __init__(self, title, data_list_list):
@@ -18,7 +18,6 @@ class LTSheet:
         self.data_frame, self.param_dict = self.get_data_frame_and_param_dict()
 
     def get_data_frame_and_param_dict(self):
-
         data_list_list, temp_list_list = [], []
         for i in range(len(self.data_list_list[0])):
             if not any(ListHelp.get_list_list_column(self.data_list_list, i)):
@@ -27,21 +26,10 @@ class LTSheet:
         temp_list_list = ListHelp.remove_list_list_none_lists(ListHelp.remove_list_list_column(temp_list_list, 0))[1:]
         param_dict = {temp_list[0]: temp_list[1] for temp_list in temp_list_list}
 
-        col_header_count = int(param_dict["col_header_count"])
-
-        return (
-            pandas.DataFrame(data_list_list[col_header_count:],
-                             columns=data_list_list[0] if col_header_count == 1 else None),
-            param_dict
-        )
+        columns = data_list_list[0] if (col_header_count := int(param_dict["col_header_count"])) == 1 else None
+        return pandas.DataFrame(data_list_list[col_header_count:], columns=columns), param_dict
 
     def sheet_to_simple_response(self):
-        ret_str = ""
-        for i, row in enumerate([list(x) for x in list(self.data_frame.values)]):
-            if i != 0:
-                ret_str += "\n"
-            for c_i, cell in enumerate([c for c in row if c]):
-                if c_i != 0:
-                    ret_str += ":   "
-                ret_str += cell
-        return ret_str
+        return ":   ".join(self.data_frame.columns) + "\n----------------------\n" + \
+               "\n".join([":   ".join([x if x is not None else "" for x in row])
+                          for row in Functions.data_frame_to_list_list(self.data_frame)])
