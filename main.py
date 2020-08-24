84401ffccefe061fae8f8204c2d34772b7a8329f
eric.wonbin.sang <eric.wonbin.sang@gmail.com>
2020-08-24 15:38:56 -0400

    Added a do_startup_message parameter for the program startup.

--- main.py
+++ main.py
@@ -6,14 +6,18 @@ from Classes.SalClasses import Sal
 def get_args():
     parser = argparse.ArgumentParser(description='This is the Stevens Automation Loader.')
     parser.add_argument('--env_key', type=str, default="dev", help='an integer for the accumulator')
-    return parser.parse_args()
+    parser.add_argument('--do_startup_message', type=str, default="true", help='Bool if bot should do startup message.')
+
+    args = parser.parse_args()
+    args.do_startup_message = args.do_startup_message.lower() == "true"
+    return args
 
 
 def main():
 
     args = get_args()
 
-    sal_bot = Sal.Sal(env_key=args.env_key)
+    sal_bot = Sal.Sal(env_key=args.env_key, do_startup_message=args.do_startup_message)
     sal_bot.watch_messages()
 
 
